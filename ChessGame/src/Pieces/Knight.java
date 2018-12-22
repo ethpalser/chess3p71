4b13c4927c049d4ab74320a74119aa8d085d3cc4
Mr.Bubbles <paramjansari0@gmail.com>
2018-12-21 22:45:52 -0500

    New Pieces Added
    Bishop, Knight, Queen

---
+++ ChessGame/src/Pieces/Knight.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pieces;
+
+import Game.Board;
+import Game.Colour;
+
+/**
+ *
+ * @author Param
+ */
+public class Knight extends Piece{
+
+    public Knight(Colour c) {
+        super(c);
+        Piece.piece = PieceType.Knight;
+        Piece.weight = 3;
+    }
+
+    @Override
+    public int heuristic(Board board) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int threats(Board board) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean[][] validMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String printToBoard() {
+       return (Piece.colour == Colour.Black) ? "BK" : "WK";
+    }
+    
+}
