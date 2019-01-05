77a23c457ef98d757dca9b3e7aa9ca241dddb92b
Mr.Bubbles <paramjansari0@gmail.com>
2019-01-05 00:59:36 -0500

    understand user input

--- ChessGame/src/Main.java
+++ ChessGame/src/Main.java
@@ -1,15 +1,38 @@
+
 import Game.*;
 import Pieces.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author E
  */
 public class Main {
-    
-    public Main(){
-    
+
+    public Main() {
+        Scanner sc;
+        while (true) {
+            sc = new Scanner(System.in);
+            String userInput = sc.next();
+            String columns = userInput.replaceAll("[^a-g]", "");
+            String rows = userInput.replaceAll("[^1-8]", "");
+            try {
+                int startC = Board.boardToIndexY(columns.charAt(0));
+                int startR = Character.getNumericValue(rows.charAt(0));
+                int nextC = Board.boardToIndexY(columns.charAt(1));
+                int nextR = Character.getNumericValue(rows.charAt(1));
+                
+                System.out.println(startC + "" + startR + "   " + nextC + "" + nextR);
+                break;
+            } catch (Exception e) {
+                System.out.println("invalid input");
+            }
+        }
     }
-    
-    public static void main(String args[]){Main m = new Main();}
+
+    public static void main(String args[]) {
+        Main m = new Main();
+    }
 }
